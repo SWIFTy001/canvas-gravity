e33daa9f80e48594eb60b093bc7b946e89aa6559
Swifty001 <93185257+SWIFTy001@users.noreply.github.com>
2025-08-22 01:41:15 +0500

    final

--- canvas.js
+++ canvas.js
@@ -5,8 +5,25 @@ canvas.height = window.innerHeight;
 
 var c = canvas.getContext('2d');
 
-var friction_y = 0.8;
-var gravity = 1.05;
+canvas.addEventListener("click",function () {
+    c.clearRect(0,0,innerWidth,innerHeight);
+    init();
+
+});
+
+addEventListener('resize', function () {
+  canvas.width = innerWidth;
+  canvas.height = innerHeight;
+  init();
+});
+
+var friction_y = 0.9;
+var friction_x = 0.9;
+var gravity = 1;
+var radius;
+var velocity_x;
+var velocity_y = 2;
+var no_of_balls = 100;
 
 function random_value (min, max) {
     return (Math.random()*(max-min))+min;
@@ -18,10 +35,11 @@ var numerals = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
 
 function random_color() {
     return '#'+numerals[Math.round(Math.random()*numerals.length)]+numerals[Math.round(Math.random()*numerals.length)]+numerals[Math.round(Math.random()*numerals.length)]+numerals[Math.round(Math.random()*numerals.length)]+numerals[Math.round(Math.random()*numerals.length)]+numerals[Math.round(Math.random()*numerals.length)];
+    //return colors_array[Math.floor(random_value(0,5))];
 }
 
 
-function Circle (x, y, dx, dy, radius, friction_x, friction_y,gravity , stroke_color, fill_color) {
+function Circle (x, y, dx, dy, radius, friction_x, friction_y,gravity, fill_color) {
     this.x = x;
     this.y = y;
     this.dx = dx;
@@ -30,51 +48,76 @@ function Circle (x, y, dx, dy, radius, friction_x, friction_y,gravity , stroke_c
     this.friction_x = friction_x;
     this.friction_y = friction_y;
     this.gravity = gravity;
-    this.stroke_color = stroke_color;
     this.fill_color = fill_color;
     this.height = y;
 
     this.draw = function () {
         c.beginPath();
         c.arc(this.x, this.y,this.radius,0,360,false);
-        c.stroke();
-        c.strokeStyle = stroke_color;
         c.fill();
+        c.stroke();
         c.fillStyle = fill_color;
 
     }
 
     this.update = function () {
+        this.draw();
+
+
         this.x += this.dx;
         this.y += this.dy;
 
-        if(this.y+this.radius >= canvas.height ) {
-            this.dy = - this.dy ;
+        if(this.y+this.radius+this.dy > canvas.height ) {
+            this.dy = - this.dy * friction_y;
         }else{
             this.dy += gravity;
+        }
+
+
+        if(this.x+this.radius+this.dx > canvas.width || this.x - this.radius +this.dx < 0) {
+            this.dx = - this.dx * this.friction_x;
+        }else if(this.dy==0){
+            this.dx *=this.friction_x;
         }
 
     }
 
 
 }
-    // var balls = [];
-
-    // for(var i=500; i<balls.length ; i++) {
-    //     balls.push(new Circle())
-    // }
-
-
-    var ball = new Circle(canvas.width/2,canvas.height/2,0,0.6,20,0,friction_y,gravity,"#000000","#000000");
     
+
+
+
+
+var balls;
+var x, y, dx, dy,color;
+
+
+
+
+
+function init() {
+    balls = [];
+    for(var i=0; i<no_of_balls ; i++) {
+        radius = random_value (8,20);
+        x = random_value (0+radius,canvas.width-radius);
+        y = random_value (0+radius,canvas.height-(radius*2));
+        velocity_x = random_value(-2,2);
+        color = random_color();
+        balls.push(new Circle(x, y,velocity_x,velocity_y,radius,friction_x,friction_y,gravity,color));
+    }   
+}    
+
+
 
 
 function animate () {
     requestAnimationFrame(animate);
-    c.clearRect(0,0,innerWidth,innerHeight)
-    ball.draw();
-    ball.update();
+    c.clearRect(0,0,innerWidth,innerHeight);
+    for(var i=0; i<no_of_balls ; i++) {
+        balls[i].update();
+    };
 }
 
-
+init();
 animate();
